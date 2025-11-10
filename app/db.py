1523affabc3eb9962bd7933227ae8b5b0bb24d55
Simphiwe <simoloijhb024@student.wethinkcode.co.za>
2025-11-10 11:09:00 +0200

    update db.py file

--- app/db.py
+++ app/db.py
@@ -1,5 +1,5 @@
-from databases import Database
-import os
+# from databases import Database
+# import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite+aiosqlite:///./expenses.db")
-database = Database(DATABASE_URL)
+# DATABASE_URL = os.getenv("DATABASE_URL", "sqlite+aiosqlite:///./expenses.db")
+# database = Database(DATABASE_URL)
